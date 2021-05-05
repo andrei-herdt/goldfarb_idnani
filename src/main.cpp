d3c35535ead71b987b32066732472b125e758e98
Andrei Herdt <andrei.herdt@tra.ai>
2021-05-05 21:14:01 +0100

    add main.cpp
    - outline main approach

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+int main {
+    // Step 0: Assume that some S-pair (x,A) is given
+    // Step 1: Repeat until all constraints are satisfied
+    // (a): Choose a violated constraint within the set of non-active
+    // constraints
+    // (b):  check for infeasibility,
+    // Stof if infeasible
+    // (c): If feasible, obtain a new solution, update the active set
+
+    return 0;
+}
