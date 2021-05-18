5fad0e8c72fbfee792a50979b6d05aeed5a3a65d
Romeo Orsolino <orsolino@arrival.com>
2021-05-18 23:01:31 +0100

    create dockerfile to run eigen

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # goldfarb_idnani
+
+To install the solver:
+sudo docker build . -t goldfarb-idnani-test:1
+
+To run the solver:
+sudo docker run --rm -it goldfarb-idnani-test:1
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-int main {
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
     // Step 0: Assume that some S-pair (x,A) is given
     // Step 1: Repeat until all constraints are satisfied
     // (a): Choose a violated constraint within the set of non-active
@@ -6,6 +10,6 @@ int main {
     // (b):  check for infeasibility,
     // Stof if infeasible
     // (c): If feasible, obtain a new solution, update the active set
-
+    std::cout << "run solver" << std::endl;
     return 0;
 }
